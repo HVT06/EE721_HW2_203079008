7264bee0535c883fa0c4201031645026e2ee0a16
HVT06 <harshvardhantiwari106@hotmail.com>
2021-08-31 19:41:40 +0530

    Update hsub_dec.vhdl

--- hsub_dec.vhdl
+++ hsub_dec.vhdl
@@ -9,7 +9,7 @@ port(A,B: in std_logic;
 end entity;
 
 architecture hsub_dec_arch1 of hsub_dec is
-signal en: std_logic;
+signal en,b1,b2: std_logic;
 signal a1: std_logic_vector(1 downto 0);
 signal a2: std_logic_vector(3 downto 0);
 
@@ -21,7 +21,9 @@ en <= '1';
 a1 <= A & B;
 
 S <= a2(0) and a2(3);
-Bout <= (a2(0) and a2(2)) and ('1' and a2(3));
+b1 <= a2(0) and a2(2);
+b2 <= '1' and a2(3);
+Bout <= b1 and b2;
 
 
 end architecture;
